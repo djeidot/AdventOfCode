05b4412ed1598a4d89b02e2095011348136da99b
João Simão <jsimao@illumina.com>
2025-02-13 14:03:54 +0000

    Refactored day6 in preparation for part 2

--- src/main/kotlin/y24day6.kt
+++ src/main/kotlin/y24day6.kt
@@ -1,16 +1,18 @@
 
 fun y24day6() {
-    val lines = readResource("y24day6.txt").toMutableList()
-    val height = lines.count()
-    val width = lines[0].count()
+    val lines = readResource("y24day6.txt").toList()
+    val matrix = lines.map { it.toCharArray() }
+    val height = matrix.count()
+    val width = matrix[0].count()
 
-    fun valueAt(pos: Position) = lines[pos.y][pos.x]
+    fun Position.getValue() = matrix[this.y][this.x]
+    fun Position.setValue(value: Char) { matrix[this.y][this.x] = value }
 
     var guard = Position(-1, -1)
 
     for (row in 0 until height) {
         for (col in 0 until width) {
-            if (lines[row][col] == '^') {
+            if (matrix[row][col] == '^') {
                 guard = Position(col, row)
             }
         }
@@ -22,46 +24,55 @@ fun y24day6() {
     while (guard.x in 0 until width && guard.y in 0 until height) {
         println("Iteration $iter, guard at $guard")
         iter++
-        var guardDirection = valueAt(guard)
-        lines[guard.y] = lines[guard.y].replaceRange(guard.x..guard.x, "X")
-        assert(lines[guard.y].count() == width)
-        var directionOk = false
-        while (!directionOk) {
-            var alternateDirection = 'x'
-            val nextPosition = guard.copy()
-            when (guardDirection) {
-                '^' -> { nextPosition.y--; alternateDirection = '>' }
-                'v' -> { nextPosition.y++; alternateDirection = '<' }
-                '>' -> { nextPosition.x++; alternateDirection = 'v' }
-                '<' -> { nextPosition.x--; alternateDirection = '^' }
-                else -> throw IllegalStateException("Unrecognized guard direction $guardDirection")
-            }
 
-            if (nextPosition.x !in 0 until width || nextPosition.y !in 0 until height) {
-                guard = nextPosition
-                break
-            }
+        fun getNextPositionAndDirection(position: Position): Pair<Position, Char>
+        {
+            var direction = position.getValue();
 
-            if (valueAt(nextPosition) == '#') {
-                guardDirection = alternateDirection
-                continue
-            }
+            var altDirection = 'x'
+            while (true) {
+                val nextPosition = position.copy();
+                when (direction) {
+                    '^' -> { nextPosition.y--; altDirection = '>' }
+                    'v' -> { nextPosition.y++; altDirection = '<' }
+                    '>' -> { nextPosition.x++; altDirection = 'v' }
+                    '<' -> { nextPosition.x--; altDirection = '^' }
+                    else -> throw IllegalStateException("Unrecognized direction ${direction}")
+                }
 
-            directionOk = true
-            guard = nextPosition
-            lines[guard.y] = lines[guard.y].replaceRange(guard.x..guard.x, "$guardDirection")
-            assert(lines[guard.y].count() == width)
-            val record = Pair(nextPosition, guardDirection)
-            if (record in previous) {
-                throw IllegalStateException("Yep, we're in a loop")
+                if (nextPosition.x !in 0 until width || nextPosition.y !in 0 until height) {
+                    return Pair(nextPosition, direction)
+                }
+
+                if (nextPosition.getValue() !in "#O") {
+                    return Pair(nextPosition, direction)
+                }
+
+                direction = altDirection
+                if (direction == position.getValue()) {
+                    throw IllegalStateException("Can't go anywhere")
+                }
             }
-            previous += record
         }
+
+        val (nextPosition, nextDirection) = getNextPositionAndDirection(guard)
+
+        if (nextPosition.x !in 0 until width || nextPosition.y !in 0 until height) {
+            break
+        }
+        //checkObstacle(guard, guardDirection)
+
+        guard.setValue('X')
+        guard = nextPosition
+        guard.setValue(nextDirection)
+        val record = Pair(nextPosition, nextDirection)
+        if (record in previous) {
+            throw IllegalStateException("Yep, we're in a loop")
+        }
+        previous += record
     }
 
-    val result = lines.sumOf { it.count { it1 -> it1 == 'X' }}
+    val result = matrix.sumOf { it.count { it1 -> it1 == 'X' }}
     print("result = $result")
-
-
 }
 
