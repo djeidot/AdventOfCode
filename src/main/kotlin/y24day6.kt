4f219f8d04281e1b436f03ddd53eb8aa6e1c208b
João Simão <jsimao@illumina.com>
2025-02-20 14:15:06 +0000

    Finished day 6 (finally!)

--- src/main/kotlin/y24day6.kt
+++ src/main/kotlin/y24day6.kt
@@ -1,70 +1,51 @@
+class Matrix(val matrix: List<CharArray>) {
+    fun getAt(row: Int, col: Int) = matrix[row][col]
+    fun getAt(pos: Position) = matrix[pos.y][pos.x]
+    fun setAt(pos: Position, value: Char) { matrix[pos.y][pos.x] = value }
+    val height get() = matrix.count()
+    val width get() = matrix[0].count()
+
+    fun copy() = Matrix(matrix.map { it.copyOf() })
+}
 
 fun y24day6() {
     val lines = readResource("y24day6.txt").toList()
-    val matrix = lines.map { it.toCharArray() }
-    val height = matrix.count()
-    val width = matrix[0].count()
-
-    fun Position.getValue() = matrix[this.y][this.x]
-    fun Position.setValue(value: Char) { matrix[this.y][this.x] = value }
+    val matrix = Matrix(lines.map { it.toCharArray() })
 
     var guard = Position(-1, -1)
 
-    for (row in 0 until height) {
-        for (col in 0 until width) {
-            if (matrix[row][col] == '^') {
+    for (row in 0 until matrix.height) {
+        for (col in 0 until matrix.width) {
+            if (matrix.getAt(row, col) == '^') {
                 guard = Position(col, row)
             }
         }
     }
 
-    val previous = mutableSetOf<Pair<Position, Char>>()
-    var iter = 1
+    val previous = mutableSetOf(Pair(guard, matrix.getAt(guard)))
 
-    while (guard.x in 0 until width && guard.y in 0 until height) {
-        println("Iteration $iter, guard at $guard")
+    var iter = 1
+    val obstacles = mutableSetOf<Position>()
+
+    while (true) {
+        println("Iteration $iter, guard at $guard facing ${matrix.getAt(guard)}")
         iter++
 
-        fun getNextPositionAndDirection(position: Position): Pair<Position, Char>
-        {
-            var direction = position.getValue();
+        val (nextPosition, nextDirection) = getNextPositionAndDirection(matrix, guard)
 
-            var altDirection = 'x'
-            while (true) {
-                val nextPosition = position.copy();
-                when (direction) {
-                    '^' -> { nextPosition.y--; altDirection = '>' }
-                    'v' -> { nextPosition.y++; altDirection = '<' }
-                    '>' -> { nextPosition.x++; altDirection = 'v' }
-                    '<' -> { nextPosition.x--; altDirection = '^' }
-                    else -> throw IllegalStateException("Unrecognized direction ${direction}")
-                }
-
-                if (nextPosition.x !in 0 until width || nextPosition.y !in 0 until height) {
-                    return Pair(nextPosition, direction)
-                }
-
-                if (nextPosition.getValue() !in "#O") {
-                    return Pair(nextPosition, direction)
-                }
-
-                direction = altDirection
-                if (direction == position.getValue()) {
-                    throw IllegalStateException("Can't go anywhere")
-                }
-            }
-        }
-
-        val (nextPosition, nextDirection) = getNextPositionAndDirection(guard)
-
-        if (nextPosition.x !in 0 until width || nextPosition.y !in 0 until height) {
+        if (nextPosition.x !in 0 until matrix.width || nextPosition.y !in 0 until matrix.height) {
+            matrix.setAt(guard, 'X')
             break
         }
-        //checkObstacle(guard, guardDirection)
 
-        guard.setValue('X')
+        if (matrix.getAt(nextPosition) != 'X') {
+            checkObstacle(matrix.copy(), guard, nextPosition, previous.map { it.copy() }.toMutableSet())
+                ?.let { obstacles.add(it) }
+        }
+
+        matrix.setAt(guard, 'X')
         guard = nextPosition
-        guard.setValue(nextDirection)
+        matrix.setAt(guard, nextDirection)
         val record = Pair(nextPosition, nextDirection)
         if (record in previous) {
             throw IllegalStateException("Yep, we're in a loop")
@@ -72,7 +53,62 @@ fun y24day6() {
         previous += record
     }
 
-    val result = matrix.sumOf { it.count { it1 -> it1 == 'X' }}
-    print("result = $result")
+    val result = matrix.matrix.sumOf { it.count { it1 -> it1 == 'X' }}
+    println("result = $result")
+    println("obstacles = ${obstacles.count()}")
+}
+
+fun checkObstacle(dreamMatrix: Matrix, guard: Position, obstacle: Position, previous: MutableSet<Pair<Position, Char>>): Position? {
+    dreamMatrix.setAt(obstacle, 'O')
+    var dreamGuard = guard.copy()
+
+    while (true) {
+        val (nextPosition, nextDirection) = getNextPositionAndDirection(dreamMatrix, dreamGuard)
+
+        if (nextPosition.x !in 0 until dreamMatrix.width || nextPosition.y !in 0 until dreamMatrix.height) {
+            return null
+        }
+
+        dreamGuard = nextPosition
+        dreamMatrix.setAt(dreamGuard, nextDirection)
+        val record = Pair(nextPosition, nextDirection)
+//        println("\tx=${nextPosition.x}, y=${nextPosition.y}, dir=$nextDirection")
+        if (record in previous) {
+//            println("\tloop")
+            return obstacle
+        }
+        previous += record
+    }
+}
+
+
+fun getNextPositionAndDirection(matrix: Matrix, position: Position): Pair<Position, Char>
+{
+    var direction = matrix.getAt(position);
+
+    var altDirection = 'x'
+    while (true) {
+        val nextPosition = position.copy();
+        when (direction) {
+            '^' -> { nextPosition.y--; altDirection = '>' }
+            'v' -> { nextPosition.y++; altDirection = '<' }
+            '>' -> { nextPosition.x++; altDirection = 'v' }
+            '<' -> { nextPosition.x--; altDirection = '^' }
+            else -> throw IllegalStateException("Unrecognized direction ${direction}")
+        }
+
+        if (nextPosition.x !in 0 until matrix.width || nextPosition.y !in 0 until matrix.height) {
+            return Pair(nextPosition, direction)
+        }
+
+        if (matrix.getAt(nextPosition) !in "#O") {
+            return Pair(nextPosition, direction)
+        }
+
+        direction = altDirection
+        if (direction == matrix.getAt(position)) {
+            throw IllegalStateException("Can't go anywhere")
+        }
+    }
 }
 
